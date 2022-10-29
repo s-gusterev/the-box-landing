2544cdd5277329d8082ad8d4a90d91c916fbe3c3
s-gusterev <copywriter-sg@yandex.ru>
2022-10-29 18:07:09 +0300

    about

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,8 @@ import hero4 from './hero-4.jpg';
 import hero5 from './hero-5.jpeg';
 import iconReputation1 from './icon-reputation-1.svg';
 import iconReputation2 from './icon-reputation-2.svg';
+import about from './about.jpg';
 
 export {
-  hero, hero2, hero3, hero4, hero5, iconReputation1, iconReputation2
+  hero, hero2, hero3, hero4, hero5, iconReputation1, iconReputation2, about
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import './About.css';
+import { about } from '../../assets';
+import SectionTitle from '../SectionTitle/SectionTitle';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className='about container'>
+      <img className='about__img' src={about} alt='about' />
+      <div className='about__content'>
+        <SectionTitle title='About us' color='light' className='about__title' />
+        <div className='about__texts'>
+          <p className='about__paragraph'>
+            For more than 30 years we have been delivering world-class
+            construction and we’ve built many lasting relationships along the
+            way.
+          </p>
+          <p className='about__paragraph'>
+            We’ve matured into an industry leader and trusted resource for those
+            seeking quality, innovation and reliability when building in the
+            U.S.
+          </p>
+        </div>
+        <a href='#' className='about__button'>
+          More on Our History
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default About;
